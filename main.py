3465ccd67f92c7ad06190eab6af38847da18c3f4
n1ck-ljj <nicholaslimcodes@gmail.com>
2022-03-09 16:23:16 +0800

    Making use of 'match' in Python 3.10

--- main.py
+++ main.py
@@ -5,11 +5,15 @@ def main():
     print("1. Static year (no changes to your salary, contribution rates and no withdrawls done)\n2. Working on it.")
     trigger = int(input(("Please select the type of calculator you want.\n")))
 
-    if trigger == 1:
-        stable_year.static_year()
-    else: 
-        print("Wrong number entered. Exiting...")
-        exit()
+    match trigger:
+        case 1:
+            stable_year.static_year()
+        case 2:
+            print("Not ready yet.")
+            exit()
+        case __: 
+            print("Wrong number entered. Exiting...")
+            exit()
 
 if __name__ == "__main__":
     main()
